ebd4277d881d90a15bf2cb3999e08b123e2bb2f1
Yuto Uchima <e245756@ie.u-ryukyu.ac.jp>
2025-07-24 15:06:23 +0900

    3rd commit

--- app.py
+++ app.py
@@ -1,9 +1,51 @@
 from flask import Flask, render_template, request
-
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 # ==================================================
 # インスタンス生成
 # ==================================================
 app = Flask(__name__)
+
+
+# ==================================================
+# Flaskに対する設定
+# ==================================================
+import os
+# 乱数を設定
+app.config['SECRET_KEY'] = os.urandom(24)
+base_dir = os.path.dirname(__file__)
+database = 'sqlite:///' + os.path.join(base_dir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# ★db変数を使用してSQLAlchemyを操作できる
+db = SQLAlchemy(app)
+# ★「flask_migrate」を使用できる様にする
+Migrate(app, db)
+
+#==================================================
+# モデル
+#==================================================
+# 課題
+class HourlyWage(db.Model):
+    # テーブル名
+    __tablename__ = 'hourly_wage'
+    
+    # 課題ID
+    HourlyWageID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # 内容
+    NormalTimeWage = db.Column(db.Integer, nullable=False)
+    NightTimeWage = db.Column(db.Integer, nullable=False)
+    MidnightTimeWage = db.Column(db.Integer, nullable=False)
+    # 完了フラグ
+    DayType = db.Column(db.Boolean, nullable=False)    
+
+    # 表示用
+    def __repr__(self):
+        return f'<HourlyWage id={self.id}, DayType={self.DayType}>'
+
 
 # ==================================================
 # ルーティング
@@ -17,6 +59,41 @@ def show_enter():
     form = SettingInfoForm(request.form)
     # POST
     if request.method == "POST" and form.validate():
+
+        weekday_wage = HourlyWage.query.filter_by(DayType=False).first()
+        if weekday_wage:
+            # 更新処理
+            weekday_wage.NormalTimeWage = form.WeekdayNormalTimeWage.data
+            weekday_wage.NightTimeWage = form.WeekdayNightTimeWage.data
+            weekday_wage.MidnightTimeWage = form.WeekdayMidnightTimeWage.data
+        else:
+            # 新規作成
+            weekday_wage = HourlyWage(
+                NormalTimeWage=form.WeekdayNormalTimeWage.data,
+                NightTimeWage=form.WeekdayNightTimeWage.data,
+                MidnightTimeWage=form.WeekdayMidnightTimeWage.data,
+                DayType=False
+            )
+            db.session.add(weekday_wage)
+
+        # 土日祝 DayType=True
+        holiday_wage = HourlyWage.query.filter_by(DayType=True).first()
+        if holiday_wage:
+            # 更新処理
+            holiday_wage.NormalTimeWage = form.HolidayNormalTimeWage.data
+            holiday_wage.NightTimeWage = form.HolidayNightTimeWage.data
+            holiday_wage.MidnightTimeWage = form.HolidayMidnightTimeWage.data
+        else:
+            # 新規作成
+            holiday_wage = HourlyWage(
+                NormalTimeWage=form.HolidayNormalTimeWage.data,
+                NightTimeWage=form.HolidayNightTimeWage.data,
+                MidnightTimeWage=form.HolidayMidnightTimeWage.data,
+                DayType=True
+            )
+            db.session.add(holiday_wage)
+        db.session.commit()
+
         return render_template('displaytest.html', form=form)
     # POST以外と「form.validate()がfalse」
     return render_template('enter.html', form=form)
@@ -26,4 +103,3 @@ def show_enter():
 # ==================================================
 if __name__ == '__main__':
     app.run()
-
--- forms.py
+++ forms.py
@@ -12,16 +12,22 @@ from wtforms.validators import (
 # ユーザー情報クラス
 class SettingInfoForm(Form):
 
+
 # 目標金額入力
     GoalAmount = IntegerField('目標金額: ', validators=[NumberRange(min=0, message='目標金額を０以上で入力してください')])
 
 # 達成目標日入力
-    AchievementDay = DateField('達成目標日: ', validators=[DataRequired('達成日は必須入力です')], format="%Y-%m-%d", render_kw={"placeholder": "yyyy/mm/dd"})
+    AchievementDay = DateField('目標達成日: ', validators=[DataRequired('達成日は必須入力です')], format="%Y-%m-%d", render_kw={"placeholder": "yyyy/mm/dd"})
 
 # 時給入力
-    NomalTimeWage = IntegerField('通常: ', validators=[NumberRange(min=0, message='時給を０以上で入力してください')])
-    NightTimeWage = IntegerField('夜間: ', validators=[NumberRange(min=0, message='時給を０以上で入力してください')])
-    MidnightTimeWage = IntegerField('深夜: ', validators=[NumberRange(min=0, message='時給を０以上で入力してください')])
+# 平日
+    WeekdayNormalTimeWage = IntegerField('通常: ', validators=[NumberRange(min=0, message='時給を０以上で入力してください')])
+    WeekdayNightTimeWage = IntegerField('夜間: ', validators=[NumberRange(min=0, message='時給を０以上で入力してください')])
+    WeekdayMidnightTimeWage = IntegerField('深夜: ', validators=[NumberRange(min=0, message='時給を０以上で入力してください')])
+# 土日祝 
+    HolidayNormalTimeWage = IntegerField('通常: ', validators=[NumberRange(min=0, message='時給を０以上で入力してください')])
+    HolidayNightTimeWage = IntegerField('夜間: ', validators=[NumberRange(min=0, message='時給を０以上で入力してください')])
+    HolidayMidnightTimeWage = IntegerField('深夜: ', validators=[NumberRange(min=0, message='時給を０以上で入力してください')])
 
 # 初期設定登録
     submit = SubmitField('決定')
